678f15d5b3d4d36bd586ef4c302800b512b57986
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-02 09:21:53 +0300

    Create ARGV.rb

---
+++ Input, output/ARGV.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+# those variables are added when running the rb program (not from input, when starting the ruby program)
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
