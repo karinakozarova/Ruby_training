5e1555a807d842e4effe4483b749dc1b15791695
Karina <karinakozarova1303@gmail.com>
2017-07-02 18:10:41 +0300

    more file training

---
+++ Files/file_all.rb
@@ -0,0 +1,19 @@
+# open file 
+filename = "file.txt" 
+# Read file
+file = open(filename)
+original_file = file.read
+	
+file.close
+# write string to end of file
+file = open(filename, "w") 
+string1 = "\nTesty input 3"
+file.write(original_file + string1) 
+
+# write another string to start of file
+string2 = "Beginning test input file_all\n"
+file.truncate(0)
+file.write(string2 + original_file + string1)
+
+#close file
+file.close()
