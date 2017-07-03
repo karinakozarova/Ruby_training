61ca12605168083f3f9f01f152517ed3d59bfe12
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-03 15:08:14 +0300

    Create problems.rb

---
+++ Codeacademy/problems.rb
@@ -0,0 +1,20 @@
+problem = false
+print "problem is false" unless problem
+
+# test_1 should be false
+test_1 = -1 == -7
+
+# test_2 = should be false
+test_2 = 0>99
+
+# test_3 = should be true
+test_3 = 0 < 1
+
+# test_1 should be true
+test_1 = 1 || 1
+
+# test_2 = should be true
+test_2 = 2 && 2
+
+# test_3 = should be false
+test_3 = 3<1 || 2<1
