0f80de067d48572c556e93f4eb72fba8dadef6d5
Karina <karinakozarova1303@gmail.com>
2017-07-03 12:14:24 +0300

    array exercise

---
+++ Arrays/numbers.rb
@@ -0,0 +1,20 @@
+puts "Normal array:"
+numbers = [0,1,2,3,4,5]
+puts numbers
+
+puts "Reversed array:"
+arr = numbers.reverse
+puts arr
+
+print "Length: "
+length = numbers.length
+puts length
+
+
+print "size: "
+size = numbers.size
+puts size
+
+puts "Ordered:"
+order = arr.sort
+puts order
