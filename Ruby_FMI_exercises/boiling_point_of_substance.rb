41783c6d9f7a66753778bbd87a18611f1da9814e
Karina <karinakozarova1303@gmail.com>
2017-07-03 11:23:33 +0300

    boiling_point_of_substance('water', 'C') #=> 100

---
+++ Ruby_FMI_exercises/boiling_point_of_substance.rb
@@ -0,0 +1,52 @@
+def convert_between_temperature_units(degrees, degrees_unit, result_unit)
+	# make degrees_unit to Celsium
+	if degrees_unit == 'C'
+		# nothing to do here
+		result = degrees
+	elsif degrees_unit == 'K'
+		#T(°C) = T(K) - 273.15
+		result = degrees - 273.15
+	else	
+		# F to C 	
+		# T(°C) = (T(°F) - 32) × 5/9
+		result = (degrees-32)*(5/9)
+	end
+
+	# celsium_unit to result_unit
+
+	if result_unit == 'C'
+		end_result = result
+	elsif result_unit == 'K'
+		end_result = result + 273.15
+
+	else	
+		# farenhait
+		#T(°F) = T(°C)*1.8 + 32
+		end_result = result*1.8 + 32
+	end
+	end_result
+
+end
+
+
+def boiling_point_of_substance(substance, result_unit)	
+	# boiling degrees
+	if substance == "water"
+		result = 100
+	elsif substance == "ethanol"
+		result = 78.37
+	elsif substance == "gold"
+		result = 2700
+	elsif substance == "silver"
+		result = 2162
+	else 
+		# copper
+		result = 2567
+	end
+	final_result = convert_between_temperature_units(result, 'C', result_unit)
+	final_result
+
+end
+
+puts boiling_point_of_substance('water', 'C') #=> 100
+puts boiling_point_of_substance('water', 'K') #=> 373.15
