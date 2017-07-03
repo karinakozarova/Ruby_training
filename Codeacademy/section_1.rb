763666533312ab08fcb21a297525e83cbeb789e7
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-03 14:48:55 +0300

    Create section_1.rb

---
+++ Codeacademy/section_1.rb
@@ -0,0 +1,18 @@
+"Test".length
+
+"String".reverse
+
+var = "sassS".upcase
+puts var.downcase
+
+name = "Me"
+
+my_name = "Me"
+my_age = 3
+
+sum = 13+379
+product = 923*15
+quotient = 13209/17
+
+name = "Kari"
+name.downcase.reverse.upcase
