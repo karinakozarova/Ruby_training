1883b913988c60fc97051b1011e18222a516b7c9
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-04 00:35:36 +0300

    Update loops_section.rb

--- Codeacademy/loops_section.rb
+++ Codeacademy/loops_section.rb
@@ -49,3 +49,44 @@ loop do
   print "#{i}\n"
  break if i <= 0
 end
+
+my_array = (1..5).to_a
+
+array = [1,2,3,4,5]
+array.each do |x|
+  x += 10
+  print "#{x}"
+end
+
+odds = [1,3,5,7,9]
+odds.each do |i|
+  i *= 2
+  print i
+end
+
+10.times { print "Chunky bacon!" }
+
+i = 1
+while i<=50
+  print i
+  i+=1
+end
+
+i = 0
+until i>=50
+  i+=1
+  print i
+end
+
+for i in 1..50
+  print i
+end
+
+count = 0
+loop do
+  print "Ruby!"
+  count += 1
+  break if count == 30
+end
+
+30.times do print "Ruby!" end
