f15efbb7254591805f18ee10e725b38eeee227e4
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-03 15:04:18 +0300

    Update section_2.rb

--- Codeacademy/section_2.rb
+++ Codeacademy/section_2.rb
@@ -43,3 +43,13 @@ test_3 = true
 
 # test_4 = 100 == 1000
 test_4 = false
+
+a = 10
+b = 11
+if a < b
+  print "a is less than b!"
+elsif b < a
+  print "b is less than a!"
+else
+  print "b is equal to a!"
+end
