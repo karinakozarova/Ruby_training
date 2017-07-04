718fc0e38b80659bda3976ed1e07a07c94b5d43e
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-04 12:19:36 +0300

    Create arrays_section.rb

---
+++ Codeacademy/arrays_section.rb
@@ -0,0 +1,16 @@
+arr = [0]
+arr << 5
+arr<<4
+my_array = arr
+
+demo_array = [100, 200, 300, 400, 500]
+print  demo_array[2]
+
+string_array = ["asdcd","sdaa"]
+
+multi_d_array = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+multi_d_array.each { |x| puts "#{x}\n" }
+
+my_2d_array = [1,2,3],[3,2,1]
+
+
