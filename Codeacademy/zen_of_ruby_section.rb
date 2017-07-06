da76d9f099bf38a273ad50cfd7d14af3d59d2bff
Karina <karinakozarova1303@gmail.com>
2017-07-06 20:44:25 +0300

    upd

--- Codeacademy/zen_of_ruby_section.rb
+++ Codeacademy/zen_of_ruby_section.rb
@@ -73,3 +73,28 @@ puts "A few of my favorite things:"
 favorite_things.each do |thing|
   puts "I love #{thing}!"
 end
+
+
+puts "One is less than two!" if 1 < 2
+
+puts 1 < 2 ?  "One is less than two!" : "One is not less than two."
+
+puts "What's your favorite language?"
+language = gets.chomp
+
+case language
+when "Ruby"
+then  puts "Ruby is great for web apps!"
+when "Python"
+ then puts "Python is great for science."
+when "JavaScript"
+  then puts "JavaScript makes websites awesome."
+when"HTML"
+  then puts "HTML is what websites are made of!"
+when"CSS"
+  then puts "CSS makes websites pretty."
+else
+  puts "I don't know that language!"
+end
+
+favorite_animal ||= "cat"
