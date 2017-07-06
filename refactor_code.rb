4642bf4ffec6c4604a3c80d9415d28f2eeeb528e
Karina <karinakozarova1303@gmail.com>
2017-07-06 20:54:55 +0300

    Added refactoring exercise

---
+++ refactor_code.rb
@@ -0,0 +1,11 @@
+require 'prime'
+
+def first_n_primes(n)
+  # Check for correct input!
+  "n must be an integer" unless n.is_a? Integer
+  "n must be greater than 0" if n <= 0
+  prime = Prime.instance
+  prime.first n
+end
+
+first_n_primes(10)
