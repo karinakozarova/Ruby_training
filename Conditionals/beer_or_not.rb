7cc69279582dd2452cd448a136fe1facf051fdb4
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-07 15:37:40 +0300

    refactor

--- Conditionals/beer_or_not.rb
+++ Conditionals/beer_or_not.rb
@@ -1,10 +1,8 @@
 puts "Enter age:"
 age = gets.to_i
-legal_age = 18
+$legal_age = 18
 
-if age<legal_age
-	puts "You can't drink"
-else
-	puts "Drink!\n"
-end
+puts "You can't drink" if age<$legal_age
+puts "Drink!\n" unless age<$legal_age
+
 
