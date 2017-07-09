f6fca538faeb5a4726012a933b8a2446cbdba20a
Karina <karinakozarova1303@gmail.com>
2017-07-09 14:54:58 +0300

    mre from the  oop section

--- Codeacademy/OOP_section.rb
+++ Codeacademy/OOP_section.rb
@@ -61,11 +61,43 @@ puts "Files: #{Computer.display_files}"
 
 #############################################################
 
+class MyClass
+  $my_variable = "Hello!"
+end
+
+puts $my_variable
 
 #############################################################
 
+class Person
+  def initialize(name,age,profession)
+    @name = name
+    @age = age
+    @profession = profession
+  end
+end
 
 #############################################################
 
+class Person
+  # Set your class variable to 0 on line 3
+  @@people_count = 0
+
+  def initialize(name)
+    @name = name
+    # Increment your class variable on line 8
+    @@people_count += 1
+  end
+
+  def self.number_of_instances
+    # Return your class variable on line 13
+    @@people_count
+  end
+end
+
+matz = Person.new("Yukihiro")
+dhh = Person.new("David")
+
+puts "Number of Person instances: #{Person.number_of_instances}"
 
 #############################################################
