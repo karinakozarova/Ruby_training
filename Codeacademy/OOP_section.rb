b7cbbebd8c868a494aaea3a1e5dbec010954b05f
Karina <karinakozarova1303@gmail.com>
2017-07-09 15:42:03 +0300

    More modules

--- Codeacademy/OOP_section.rb
+++ Codeacademy/OOP_section.rb
@@ -431,3 +431,18 @@ end
 TheHereAnd.now
 
 #############################################################
+
+class Application
+  attr_accessor :status
+  def initialize; end
+    public
+    def print_status
+        puts "All systems go!"
+    end
+
+end
+
+#############################################################
+
+
+#############################################################
