0c7761a4c726109c0017ba7775c7cfc72a5c4564
Karina <karinakozarova1303@gmail.com>
2017-07-09 14:36:17 +0300

    removed old testing files

--- Codeacademy/test.rb
+++
@@ -1,5 +0,0 @@
-my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
-
-# Add your code below!
-symbol_filter = lambda { |x| x.capitalize.is_a? Symbol}
-symbols = my_array.select(&symbol_filter)
