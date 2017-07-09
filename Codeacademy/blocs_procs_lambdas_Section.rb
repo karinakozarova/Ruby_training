9a83176ae4526144106cdef0bfce30f333de8475
Karina <karinakozarova1303@gmail.com>
2017-07-09 14:42:34 +0300

    completed section

--- Codeacademy/blocs_procs_lambdas_Section.rb
+++ Codeacademy/blocs_procs_lambdas_Section.rb
@@ -134,4 +134,36 @@ my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
 symbol_filter = lambda { |x| x.capitalize.is_a? Symbol}
 symbols = my_array.select(&symbol_filter)
 
-##########################################################
+################################################################
+# Reviewing section starts here
+################################################################
+
+odds_n_ends = [:weezard, 42, "Trady Blix", 3, true, 19, 12.345]
+ints = odds_n_ends.select {|x| x.is_a? Integer }
+
+################################################################
+
+ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
+under_100 = Proc.new {|x| if x<100 then true end }
+
+################################################################
+
+ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
+under_100 = Proc.new {|x| if x<100 then true end }
+youngsters = ages.select(&under_100)
+
+################################################################
+
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+first_half = lambda {|key,value| value < 'M'}
+a_to_m = crew.select(&first_half)
+
