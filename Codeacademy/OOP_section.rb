701e28385f4cfba4d401ef0dfecaa2077cf3c4e9
Karina <karinakozarova1303@gmail.com>
2017-07-09 15:12:41 +0300

    more oop

--- Codeacademy/OOP_section.rb
+++ Codeacademy/OOP_section.rb
@@ -273,3 +273,44 @@ class Dog
 end
 
 #############################################################
+
+class Person
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+
+  def name
+    @name
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+end
+
+#############################################################
+
+class Person
+  attr_reader :name
+  attr_writer :job
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+end
+
+#############################################################
+
+class Person
+  attr_reader :name
+  attr_accessor :job
+
+
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+end
+
+#############################################################
