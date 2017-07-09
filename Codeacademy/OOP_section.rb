f6698c36455df303e3cf467700a2113241c07729
Karina <karinakozarova1303@gmail.com>
2017-07-09 15:03:04 +0300

    more oop

--- Codeacademy/OOP_section.rb
+++ Codeacademy/OOP_section.rb
@@ -204,11 +204,28 @@ class Message
             @to = to
             @@messages_sent += 1
     end
-
 end
+
+my_message = Message.new("Kari", "You")
 
 #############################################################
 
+class Message
+@@messages_sent = 0
+    def initialize (from,to)
+            @from = from
+            @to = to
+            @@messages_sent += 1
+    end
+end
+
+class Email < Message
+
+    def initialize(subject)
+        @subject = subject
+    end
+end
+my_message = Message.new("Kari", "You")
 
 #############################################################
 
