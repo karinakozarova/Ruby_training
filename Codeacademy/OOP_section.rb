0777b361212febe8376f575f12d9c108ac085d01
Karina <karinakozarova1303@gmail.com>
2017-07-09 15:43:32 +0300

    final modules section commit

--- Codeacademy/OOP_section.rb
+++ Codeacademy/OOP_section.rb
@@ -435,14 +435,38 @@ TheHereAnd.now
 class Application
   attr_accessor :status
   def initialize; end
+      private
+      def password
+          12345
+      end
     public
     def print_status
         puts "All systems go!"
-    end
+end
 
+end
+#############################################################
+
+module Languages
+FAVE = "C"
 end
 
 #############################################################
 
+module Languages
+  FAVE = "Ruby"  # This is what you typed before, right? :D
+end
+
+class Master
+include Languages
+  def initialize; end
+  def victory
+    puts FAVE
+  end
+end
+
+total = Master.new
+total.victory
 
 #############################################################
+
