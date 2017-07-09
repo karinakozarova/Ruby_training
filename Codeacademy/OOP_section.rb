fa6e728b914585ccb742adc8a9db63c85e23743f
Karina <karinakozarova1303@gmail.com>
2017-07-09 14:51:57 +0300

    started oop section

---
+++ Codeacademy/OOP_section.rb
@@ -0,0 +1,71 @@
+class Language
+  def initialize(name, creator)
+    @name = name
+    @creator = creator
+  end
+
+  def description
+    puts "I'm #{@name} and I was created by #{@creator}!"
+  end
+end
+
+ruby = Language.new("Ruby", "Yukihiro Matsumoto")
+python = Language.new("Python", "Guido van Rossum")
+javascript = Language.new("JavaScript", "Brendan Eich")
+
+ruby.description
+python.description
+javascript.description
+
+#############################################################
+
+class Person
+    def initialize(name)
+        @name = name
+    end
+end
+
+matz =  Person.new("Yukihiro")
+
+#############################################################
+
+class Computer
+  $manufacturer = "Mango Computer, Inc."
+  @@files = {hello: "Hello, world!"}
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+  end
+
+  def current_user
+    @username
+  end
+
+  def self.display_files
+    @@files
+  end
+end
+
+# Make a new Computer instance:
+hal = Computer.new("Dave", 12345)
+
+puts "Current user: #{hal.current_user}"
+# @username belongs to the hal instance.
+
+puts "Manufacturer: #{$manufacturer}"
+# $manufacturer is global! We can get it directly.
+
+puts "Files: #{Computer.display_files}"
+# @@files belongs to the Computer class.
+
+#############################################################
+
+
+#############################################################
+
+
+#############################################################
+
+
+#############################################################
